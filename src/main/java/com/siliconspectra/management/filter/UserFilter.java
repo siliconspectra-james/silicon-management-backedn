b5660c9781f72a16b41c068aac9958514f921876
james <you@example.com>
2023-12-10 19:38:41 -0800

    enable filter

--- src/main/java/com/siliconspectra/management/filter/UserFilter.java
+++ src/main/java/com/siliconspectra/management/filter/UserFilter.java
@@ -16,8 +16,8 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-//@Component
-//@WebFilter
+@Component
+@WebFilter
 public class UserFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
