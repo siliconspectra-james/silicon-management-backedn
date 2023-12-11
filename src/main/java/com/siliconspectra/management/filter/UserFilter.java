0e10f339df4e9a0b0a4e6ed27ef437c066d8a829
siliconspectra-james <54572896+siliconspectra-james@users.noreply.github.com>
2023-12-10 21:09:48 -0800

    Update UserFilter.java
    update cors origin

--- src/main/java/com/siliconspectra/management/filter/UserFilter.java
+++ src/main/java/com/siliconspectra/management/filter/UserFilter.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Component
 @WebFilter
 public class UserFilter implements Filter {
