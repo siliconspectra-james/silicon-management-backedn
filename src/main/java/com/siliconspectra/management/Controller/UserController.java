c6ac778ae5a4881efe143055194aaac4a79c9a65
gongzhenmu <gongzhenmu@users.noreply.github.com>
2023-12-14 12:49:13 -0800

    Admin updateUser and return data type fixed

--- src/main/java/com/siliconspectra/management/Controller/UserController.java
+++ src/main/java/com/siliconspectra/management/Controller/UserController.java
@@ -20,35 +20,51 @@ public class UserController {
 
     @Autowired
     UserService userService;
+    //Get single user data in Candidate form.
     @GetMapping("/candidate/{uid}")
     public Candidate getCandidateById(@PathVariable String uid) throws CustomException {
         return userService.getCandidateById(uid);
     }
-
+    //Update user data in Candidate form
     @PutMapping("/candidate/{uid}")
     public ResponseEntity<Response> updateCandidate(@PathVariable String uid, @RequestBody Candidate c) throws CustomException{
-        userService.updateUser(uid,c);
+        userService.updateCandidate(uid,c);
+        return new ResponseEntity<>(new Response("User update success"), HttpStatus.OK);
+    }
+    //Update any user data with admin account.
+    @PutMapping("/admin/{uid}")
+    public ResponseEntity<Response> adminUpdateUser(@PathVariable String uid, @RequestBody User user) throws CustomException{
+        userService.updateUser(uid,user);
         return new ResponseEntity<>(new Response("User update success"), HttpStatus.OK);
     }
 
+    //Get single user with admin access.
+    @GetMapping("/admin/{uid}")
+    public User adminGetUserById(@PathVariable String uid) throws CustomException {
+        return userService.getUserById(uid);
+    }
+
+
+    //Create new user data (Admin access)
     @PostMapping("/admin/{uid}")
     public ResponseEntity<Response> createUser(@PathVariable String uid, @RequestBody User user) throws CustomException {
         String message = userService.createUser(user);
         return new ResponseEntity<>(new Response(message), HttpStatus.OK);
     }
-
-    @GetMapping("/admin/all")
+    //Get all user data in Candidate form.
+    @GetMapping("/admin/all/candidates")
     public List<Candidate> getAllCandidates()throws CustomException{
 
         return userService.getAllCandidates();
     }
 
-    //This is for test
-//    @GetMapping("/admin/alluser")
-//    public List<User> getAllUsers()throws CustomException{
-//
-//        return userService.getAllUsers();
-//    }
+    //Get all user data in User form.
+    @GetMapping("/admin/all/users")
+    public List<User> getAllUsers()throws CustomException{
+
+        return userService.getAllUsers();
+    }
+    //Delete user data in database.
     @DeleteMapping("/admin/delete/{uid}")
     public ResponseEntity<Response> deleteUser(@PathVariable String uid) throws  Exception{
         userService.deleteUserById(uid);
